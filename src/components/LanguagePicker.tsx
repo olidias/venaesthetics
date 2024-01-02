4ad0fff087f89015a338f29fff1089bd3705f0af
Oli Dias <info@dscs.io>
2024-01-02 17:37:28 +0100

    initial commit

---
+++ src/components/LanguagePicker.tsx
@@ -0,0 +1,87 @@
+import { languages } from "../data/ui";
+import { Icon } from "@iconify/react";
+import { getLangFromUrl } from "../data/utils";
+import { useState, useEffect, useRef } from "react";
+import React from "react";
+
+export default function LanguagePicker({ url }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  function toggleMenu() {
+    setIsComponentVisible(true);
+    setMenuOpen(!menuOpen);
+  }
+  const lang = languages.find(l => l.key === getLangFromUrl(url));
+
+  const [isComponentVisible, setIsComponentVisible] = useState(true);
+  const ref = useRef(null);
+
+  const handleHideDropdown = (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      setIsComponentVisible(false);
+      setMenuOpen(false);
+    }
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsComponentVisible(false);
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleHideDropdown, true);
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("keydown", handleHideDropdown, true);
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  });
+
+  return (
+    <div className="relative inline-block text-left" ref={ref}>
+      <div>
+        <button
+          type="button"
+          id="languageDropdownButton"
+          className="text-muted mr-2 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 inline-flex items-center"
+          aria-expanded="true"
+          aria-haspopup="true"
+          onClick={toggleMenu}
+        >
+          <Icon icon={lang?.icon} className="h-5 w-5 " />
+        </button>
+      </div>
+      {isComponentVisible && (
+        <div
+          className={`${
+            menuOpen ? "" : "hidden"
+          } absolute right-0 z-10 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-primary-dark shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+          id="languageDropdown"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabIndex="-1"
+        >
+          <div className="py-1 flex-row" role="none">
+            {languages.map((language, i) => (
+              <a
+                key={i}
+                href={`/${language.key}`}
+                className="text-gray-700 px-4 py-2 text-sm flex justify-items-start hover:bg-gray-100"
+                role="menuitem"
+                tabIndex="-1"
+                id="menu-item-0"
+              >
+                <div className="flex-col flex w-5 h-5 items-center justify-center">
+                  <Icon icon={language?.icon} className="flex mr-2" />
+                </div>
+                <span className="align-middle">{language.label}</span>
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/data/ui.ts
@@ -0,0 +1,27 @@
+export const languages = [
+    {
+      key: 'en',
+      label: 'English',
+      icon: 'emojione:flag-for-united-kingdom',
+    },
+    {
+      key: 'de',
+      label: 'Deutsch',
+      icon: 'emojione:flag-for-switzerland',
+    },
+  ];
+  
+export const defaultLang = 'de';
+
+export const ui = {
+    en: {
+        'links.competences': 'Competences',
+        'links.references': 'References',
+        'links.contact': 'Contact'
+    },
+    de: {
+        'links.competences': 'Kompetenzen',
+        'links.references': 'Referenzen',
+        'links.contact': 'Kontakt',
+    }
+}
