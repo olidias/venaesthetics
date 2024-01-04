b75080937d1adfb9df0479bcacff00a9c7e44084
Oli Dias <info@dscs.io>
2024-01-04 18:01:19 +0100

    first version references & refactoring dato-client

---
+++ src/common/dato-client.ts
@@ -0,0 +1,25 @@
+import { buildClient, LogLevel } from "@datocms/cma-client-node";
+import type { Lang } from "./constants";
+
+export async function getItemsByFilter(filter: string, lang: Lang) {
+    return await client.items.list({
+        filter: {
+          type: filter,
+        },
+        locale: lang
+      });
+}
+
+export async function getUploadsByIds(ids: string[]) {
+    const idsParam = ids.join(',');
+    return await client.uploads.list({
+        filter: {
+            ids: idsParam
+        }
+    });
+}
+
+const client = buildClient({
+    apiToken: import.meta.env.DATO_CMS_API_KEY,
+    logLevel: LogLevel.BASIC,
+ });
